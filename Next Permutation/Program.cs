b845b9e0dc5130b0296e18d8a4262bdc3e2d34d2
Amit Sharma <amitsharma171094@outlook.com>
2021-11-12 03:27:20 +0530

    Rotate Matrix and Next Permutation Problems

---
+++ Next Permutation/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Next_Permutation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static List<int> NextPermutation(List<int> A)
+        {
+            int i, j;
+
+            // First find the smallest number from end of the list
+            for (i = A.Count - 2; i >= 0; i--)
+            {
+                if (A[i] < A[i + 1])
+                    break;
+            }
+
+            // If smallest number is not find, return the reverse or sort of the given list
+            if (i == -1)
+            {
+                A.Sort();
+                return A;
+            }
+
+            // Now find a number larger than the smallest number which we found above again from the end of the list
+            for (j = A.Count - 1; j > 0; j--)
+            {
+                if (A[j] > A[i])
+                    break;
+            }
+
+            // Swap these 2 numbers
+            int temp = A[j];
+            A[j] = A[i];
+            A[i] = temp;
+
+            // Sort the in between numbers and the return the list
+            var tempList = A.GetRange(i + 1, A.Count - (i + 1));
+            A.RemoveRange(i + 1, A.Count - (i + 1));
+            tempList.Sort();
+            A.AddRange(tempList);
+            return A;
+        }
+
+        static void Main(string[] args)
+        {
+            NextPermutation(new List<int> { 1, 1, 1, 2, 3, 5, 9, 8, 7, 6 }).ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ Rotate Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Rotate_Matrix
+{
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static void Rotate(List<List<int>> A)
+        {
+            // First Transpose, then Swap Elements
+            // Transpose
+            for (int i = 0; i < A.Count; i++)
+            {
+                for (int j = i; j < A.Count; j++)
+                {
+                    int temp = A[i][j];
+                    A[i][j] = A[j][i];
+                    A[j][i] = temp;
+                }
+            }
+
+            // Now Swap Elements
+            for(int i = 0; i < A.Count; i++)
+            {
+                for (int j = 0; j < A.Count/2; j++)
+                {
+                    int temp = A[i][j];
+                    A[i][j] = A[i][A.Count - 1 - j];
+                    A[i][A.Count - 1 - j] = temp;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var list1 = new List<int> { 1, 2, 3 };
+            var list2 = new List<int> { 4, 5, 6 };
+            var list3 = new List<int> { 7, 8, 9 };
+            var list = new List<List<int>>
+            {
+                list1,
+                list2,
+                list3
+            };
+            Rotate(list);
+        }
+    }
+}
