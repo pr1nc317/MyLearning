560ffa6b60fdc1db4c784c37d92f1053808767d9
Amit Sharma <shaami@microsoft.com>
2021-11-12 15:14:05 +0530

    Added Find Permutation problem

---
+++ Find Permutation/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Find_Permutation
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int[] FindPermutation(string A, int B)
+        {
+            int[] arr = new int[B];
+            int start = 1;
+            int end = B;
+            int i = 0;
+            for (i = 0; i < A.Length; i++)
+            {
+                if(A[i] == 'I')
+                {
+                    arr[i] = start++;
+                }
+                else
+                {
+                    arr[i] = end--;
+                }
+            }
+            arr[i] = start;
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            var ans = FindPermutation("IDI", 4);
+            foreach (var item in ans)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
