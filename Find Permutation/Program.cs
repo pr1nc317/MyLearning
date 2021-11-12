cdddb987739191b78b55f4109a746c0aaf7abd53
Amit Sharma <shaami@microsoft.com>
2021-11-12 20:48:43 +0530

    Addedd different variations of Find Premutation problem

--- Find Permutation/Program.cs
+++ Find Permutation/Program.cs
@@ -7,23 +7,77 @@
     {
         public static int[] FindPermutation(string A, int B)
         {
-            int[] arr = new int[B];
-            int start = 1;
-            int end = B;
-            int i = 0;
+            #region Solution 1 - using array
+            //int[] arr = new int[B];
+            //int start = 1;
+            //int end = B;
+            //int i;
+            //for (i = 0; i < A.Length; i++)
+            //{
+            //    if(A[i] == 'I')
+            //    {
+            //        arr[i] = start++;
+            //    }
+            //    else
+            //    {
+            //        arr[i] = end--;
+            //    }
+            //}
+            //arr[i] = start;
+            //return arr;
+            #endregion
+
+            #region Solution 2 - using stack
+            //Stack<int> stack = new Stack<int>();
+            //var ans = new List<int>();
+            //int i;
+            //for (i = 0; i < A.Length; i++)
+            //{
+            //    stack.Push(i);
+            //    if (A[i] == 'I')
+            //    {
+            //        while(stack.Count > 0)
+            //        {
+            //            ans.Add(stack.Pop());
+            //        }
+            //    }
+            //}
+
+            //stack.Push(i);
+
+            //while (stack.Count > 0)
+            //{
+            //    ans.Add(stack.Pop());
+            //}
+
+            //return ans.ToArray();
+            #endregion
+
+            #region Solution 3 - using simple variables
+            int i, j, start = 1, end = 0;
+            var ans = new List<int>();
             for (i = 0; i < A.Length; i++)
             {
-                if(A[i] == 'I')
+                end++;
+                if (A[i] == 'I')
                 {
-                    arr[i] = start++;
-                }
-                else
-                {
-                    arr[i] = end--;
+                    for (j = end; j >= start; j--)
+                    {
+                        ans.Add(j);
+                    }
+                    start = end + 1;
                 }
             }
-            arr[i] = start;
-            return arr;
+            
+            ans.Add(end + 1);
+
+            for (j = end; j >= start; j--)
+            {
+                ans.Add(A[j]);
+            }
+
+            return ans.ToArray();
+            #endregion
         }
 
         static void Main(string[] args)
