fea150197b874fbaaf36816843b6c204d38a60d9
Amit Sharma <shaami@microsoft.com>
2021-11-17 21:42:32 +0530

    Added Pascal Triangle problem of Arrays

--- Pascal Triangle/Program.cs
+++ Pascal Triangle/Program.cs
@@ -1,12 +1,40 @@
 ﻿namespace Pascal_Triangle
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
+        public static List<List<int>> Solve(int A)
+        {
+            // Using the logic of nCr, we can find first element is nothing but nC0, then nC1 .... nCn
+            // And nCr = (nCr-1 * (n - r + 1))/r
+
+            var list = new List<List<int>>();
+
+            for (int i = 0; i < A; i++)
+            {
+                var tempList = PascalTriangle(i);
+                list.Add(tempList);
+            }
+            return list;
+        }
+
+        public static List<int> PascalTriangle(int A)
+        {
+            int prev = 1;
+            var tempList = new List<int> { prev };
+            for (int i = 1; i <= A; i++)
+            {
+                prev = (prev * (A - i + 1)) / i;
+                tempList.Add(prev);
+            }
+            return tempList;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solve(4).ForEach(x => { x.ForEach(y => Console.Write(" " + y)); Console.WriteLine(); });
         }
     }
 }
