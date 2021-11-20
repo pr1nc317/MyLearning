af71d77c68b13146abeeef439ddfb55418a538b0
Amit Sharma <amitsharma171094@outlook.com>
2021-11-21 00:11:11 +0530

    Added another solutions for 'Balance Array' and 'Triplets with sum  between given range' problems of Arrays

--- Balance Array/Program.cs
+++ Balance Array/Program.cs
@@ -7,39 +7,90 @@
     {
         public static int Solve(List<int> A)
         {
+            #region SOlution 1
+            //int n = A.Count;
+            //int sumEven = 0; int sumOdd = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        sumEven += A[i];
+            //    }
+            //    else
+            //    {
+            //        sumOdd += A[i];
+            //    }
+            //}
+
+            //int kEven = 0, kOdd = 0, ans = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        if (sumEven - A[i] - kEven + kOdd == sumOdd - kOdd + kEven)
+            //            ans++;
+            //        kEven += A[i];
+            //    }
+            //    else
+            //    {
+            //        if (sumOdd - A[i] - kOdd + kEven == sumEven - kEven + kOdd)
+            //            ans++;
+            //        kOdd += A[i];
+            //    }
+            //}
+            //return ans;
+            #endregion
+
+            #region Solution 2
             int n = A.Count;
-            int sumEven = 0; int sumOdd = 0;
+            int sum1 = 0, sum2 = 0;
+
+            // sum even and odd elements
             for (int i = 0; i < n; i++)
             {
-                if (i%2==0)
+                if (i % 2 == 0)
                 {
-                    sumEven += A[i];
+                    sum1 += A[i];
                 }
                 else
                 {
-                    sumOdd += A[i];
+                    sum2 += A[i];
                 }
             }
 
-            int kEven = 0, kOdd = 0, ans = 0;
+            int result = 0;
+            int prev1 = 0, prev2 = 0;
 
             for (int i = 0; i < n; i++)
             {
-                if (i%2==0)
+                if (i % 2 == 0)
                 {
-                    if (sumEven - A[i] - kEven + kOdd == sumOdd - kOdd + kEven)
-                        ans++;
-                    kEven += A[i];
+                    int even = prev1 + (sum2 - prev2);
+                    int odd = prev2 + (sum1 - prev1 - A[i]);
+
+                    if (odd == even)
+                    {
+                        result++;
+                    }
+
+                    prev1 += A[i];
                 }
                 else
                 {
-                    if (sumOdd - A[i] - kOdd + kEven == sumEven - kEven + kOdd)
-                        ans++;
-                    kOdd += A[i];
+                    int even = prev1 + (sum2 - prev2 - A[i]);
+                    int odd = prev2 + (sum1 - prev1);
+
+                    if (odd == even)
+                    {
+                        result++;
+                    }
+
+                    prev2 += A[i];
                 }
             }
-
-            return ans;
+            return result;
+            #endregion
         }
 
         static void Main(string[] args)
--- Triplets with Sum between given range/Program.cs
+++ Triplets with Sum between given range/Program.cs
@@ -7,43 +7,101 @@
     {
         public static int Solve(List<string> A)
         {
-            int n = A.Count;
-            double[] arr = new double[n];
+            #region Solution 1
+            //int n = A.Count;
+            //double[] arr = new double[n];
 
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = Convert.ToDouble(A[i]);
-            }
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr[i] = Convert.ToDouble(A[i]);
+            //}
 
-            double a = arr[0], b = arr[1], c = arr[2];
+            //double a = arr[0], b = arr[1], c = arr[2];
             
-            for (int i = 3; i < n; i++)
+            //for (int i = 3; i < n; i++)
+            //{
+            //    if (a + b + c > 1 && a + b + c < 2)
+            //        return 1;
+            //    else if (a+b+c > 2)
+            //    {
+            //        if (a > b && a > c)
+            //            a = arr[i];
+            //        else if (b > a && b > c)
+            //            b = arr[i];
+            //        else
+            //            c = arr[i];
+            //    }
+            //    else
+            //    {
+            //        if (a < b && a < c)
+            //            a = arr[i];
+            //        else if (b < a && b < c)
+            //            b = arr[i];
+            //        else
+            //            c = arr[i];
+            //    }
+            //}
+
+            //if (a + b + c > 1 && a + b + c < 2)
+            //    return 1;
+            //else return 0;
+            #endregion
+
+            #region Solution 2
+            List<double> parsedval = new List<double>();
+            for (int i = 0; i < A.Count; i++)
             {
-                if (a + b + c > 1 && a + b + c < 2)
-                    return 1;
-                else if (a+b+c > 2)
+                parsedval.Add(Convert.ToDouble(A[i]));
+            }
+            List<double> firstBucket = new List<double>();
+            List<double> secondBucket = new List<double>();
+            List<double> thirdBucket = new List<double>();
+            for (int i = 0; i < parsedval.Count; i++)
+            {
+                if (parsedval[i] < (double)2 / 3)
                 {
-                    if (a > b && a > c)
-                        a = arr[i];
-                    else if (b > a && b > c)
-                        b = arr[i];
-                    else
-                        c = arr[i];
+                    firstBucket.Add(parsedval[i]);
                 }
                 else
+                if (parsedval[i] >= (double)2 / 3 && parsedval[i] <= 1)
                 {
-                    if (a < b && a < c)
-                        a = arr[i];
-                    else if (b < a && b < c)
-                        b = arr[i];
-                    else
-                        c = arr[i];
+                    secondBucket.Add(parsedval[i]);
+                }
+                else if (parsedval[i] > 1 && parsedval[i] < 2)
+                {
+                    thirdBucket.Add(parsedval[i]);
                 }
             }
+            firstBucket.Sort();
+            secondBucket.Sort();
+            thirdBucket.Sort();
 
-            if (a + b + c > 1 && a + b + c < 2)
+            if (firstBucket.Count >= 3 && (firstBucket[firstBucket.Count - 1] + firstBucket[firstBucket.Count - 2] + firstBucket[firstBucket.Count - 3] >= 1))
+            {
                 return 1;
-            else return 0;
+            }
+            if (firstBucket.Count >= 2 && thirdBucket.Count >= 1 && (firstBucket[0] + firstBucket[0] + thirdBucket[0] <= 2))
+            {
+                return 1;
+            }
+            if (firstBucket.Count >= 1 && secondBucket.Count >= 2 && (firstBucket[0] + secondBucket[0] + secondBucket[1] <= 2))
+            {
+                return 1;
+            }
+            if (firstBucket.Count >= 1 && secondBucket.Count >= 1 && thirdBucket.Count >= 1 && (firstBucket[0] + secondBucket[0] + thirdBucket[0] <= 2))
+            {
+                return 1;
+            }
+            if (firstBucket.Count >= 2 && secondBucket.Count >= 1 && (firstBucket[firstBucket.Count - 1] + firstBucket[firstBucket.Count - 2] + secondBucket[0] < 2) && (firstBucket[firstBucket.Count - 1] + firstBucket[firstBucket.Count - 2] + secondBucket[0] > 1))
+            {
+                return 1;
+            }
+            if (firstBucket.Count >= 2 && secondBucket.Count >= 1 && (firstBucket[0] + firstBucket[1] + secondBucket[secondBucket.Count - 1] > 1) && (firstBucket[0] + firstBucket[1] + secondBucket[secondBucket.Count - 1] < 2))
+            {
+                return 1;
+            }
+            return 0;
+            #endregion
         }
 
         static void Main(string[] args)
