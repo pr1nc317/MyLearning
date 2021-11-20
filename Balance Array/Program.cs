e4fa2f09b23b13b7ffcc9494264cf7b40589185d
Amit Sharma <amitsharma171094@outlook.com>
2021-11-20 23:47:36 +0530

    Added 'Balance Array' and 'Find Duplicate in Array' problem of Arrays

---
+++ Balance Array/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Balance_Array
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int Solve(List<int> A)
+        {
+            int n = A.Count;
+            int sumEven = 0; int sumOdd = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i%2==0)
+                {
+                    sumEven += A[i];
+                }
+                else
+                {
+                    sumOdd += A[i];
+                }
+            }
+
+            int kEven = 0, kOdd = 0, ans = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i%2==0)
+                {
+                    if (sumEven - A[i] - kEven + kOdd == sumOdd - kOdd + kEven)
+                        ans++;
+                    kEven += A[i];
+                }
+                else
+                {
+                    if (sumOdd - A[i] - kOdd + kEven == sumEven - kEven + kOdd)
+                        ans++;
+                    kOdd += A[i];
+                }
+            }
+
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(new List<int> { 1,2,1,1}));
+        }
+    }
+}
---
+++ Find Duplicate in Array/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Find_Duplicate_in_Array
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int RepeatedNumber(List<int> A)
+        {
+            #region Solution 1 - Using Dictionary
+            //int n = A.Count;
+            //var dict = new Dictionary<int, bool>();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (dict.ContainsKey(A[i]))
+            //        return A[i];
+            //    dict.Add(A[i], false);
+            //}
+            //return -1;
+            #endregion
+
+            #region Using bool array
+            bool[] flagArray = new bool[A.Count];
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (flagArray[A[i] - 1])
+                {
+                    return A[i];
+                }
+                flagArray[A[i] - 1] = true;
+            }
+            return -1;
+            #endregion
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatedNumber(new List<int> { 2, 4, 3, 1, 0 }));
+        }
+    }
+}
