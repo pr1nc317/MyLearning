9f2f9495fa76c21f83b7263fe20a70039b07a664
Amit Sharma <amitsharma171094@outlook.com>
2021-11-24 23:29:29 +0530

    Added 'All Factors', 'Prime Numbers' and 'Verify Prime' problems of Math

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Prime_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static List<int> Sieve(int A)
+        {
+            var arr = new bool[A + 1];
+            var list = new List<int>();
+            for (int i = 1; i <= A; i++)
+            {
+                arr[i] = true;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(A); i++)
+            {
+                for (int k = i * i; k <= A; k += i)
+                {
+                    arr[k] = false;
+                }
+            }
+
+            for (int i = 2; i <= A; i++)
+            {
+                if (arr[i] == true)
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            Sieve(10).ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
