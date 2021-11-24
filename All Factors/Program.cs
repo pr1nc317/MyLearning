00fee672336e1ebbcbac8082f92c665922de59af
Amit Sharma <amitsharma171094@outlook.com>
2021-11-24 23:29:29 +0530

    Added 'All Factors', 'Prime Numbers' and 'Verify Prime' problems of Arrays

---
+++ All Factors/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace All_Factors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static List<int> AllFactors(int A)
+        {
+            HashSet<int> ans = new HashSet<int>();
+            int start;
+            int end = A;
+            for (start = 1; start < end; start++)
+            {
+                if (A % start == 0)
+                {
+                    ans.Add(start);
+                    end = A / start;
+                    ans.Add(end);
+                }
+            }
+            var ans1 = ans.ToList();
+            ans1.Sort();
+            return ans1;
+        }
+
+        static void Main(string[] args)
+        {
+            AllFactors(82944).ForEach(x=>Console.WriteLine(x));
+        }
+    }
+}
---
+++ Verify Prime/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Verify_Prime
+{
+    using System;
+
+    class Program
+    {
+        public static int IsPrime(int A)
+        {
+            int start;
+            if (A == 1) return 0;
+            for (start = 2; start <= Math.Sqrt(A); start++)
+            {
+                if (A % start == 0)
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrime(9));
+        }
+    }
+}
