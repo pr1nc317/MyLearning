be7785768f729fd6ec65c725fb1fc0035b936286
Amit Sharma <amitsharma171094@outlook.com>
2021-11-25 22:51:17 +0530

    Added 'Binary Representation' problem of Math

---
+++ Binary Representation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Binary_Representation
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static string FindDigitsInBinary(int A)
+        {
+            string ans = "";
+            var list = new List<string>();
+            while(A>=1)
+            {
+                list.Insert(0, (A % 2).ToString());
+                A /= 2;
+            }
+            list.ForEach(x => ans += x);
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindDigitsInBinary(3));
+        }
+    }
+}
