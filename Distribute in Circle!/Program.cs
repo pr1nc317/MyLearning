76b2c2133c3f2c4f7cb0fd70b3b4950124e65f9d
Amit Sharma <shaami@microsoft.com>
2021-11-26 20:17:05 +0530

    Added 'Distribute in Circle!' problem of Math

---
+++ Distribute in Circle!/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Distribute_in_Circle_
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A, int B, int C)
+        {
+            return (A + C - 1) % B;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(8,5,2));
+        }
+    }
+}
