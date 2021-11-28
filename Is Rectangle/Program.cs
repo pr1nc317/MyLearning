536ba36189d72e2c909f518489ae8a05f9c824c7
Amit Sharma <amitsharma171094@outlook.com>
2021-11-28 20:08:58 +0530

    Added 'Is Rectangle?' problem of Math

---
+++ Is Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Is_Rectangle
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A, int B, int C, int D)
+        {
+            if (A == B && C == D)
+                return 1;
+            else if (A == C & B == D)
+                return 1;
+            else if (A == D & B == C)
+                return 1;
+            else
+                return 0;
+        }
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(Solve(1,1,4,4));
+        }
+    }
+}
