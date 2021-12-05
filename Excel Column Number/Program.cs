a4e675125733f8ed3f9254b254e66c98111c65a6
Amit Sharma <amitsharma171094@outlook.com>
2021-12-05 20:32:56 +0530

    Added 'Excel_Column_Number' problem of Math

---
+++ Excel Column Number/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Excel_Column_Number
+{
+    using System;
+
+    class Program
+    {
+        public static int TitleToNumber(string A)
+        {
+            var arr = A.ToCharArray();
+            int ans = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                var num = (int)arr[i] - 64;
+                ans += (int)Math.Pow(26, arr.Length - 1 - i) * num;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TitleToNumber("BA"));
+        }
+    }
+}
