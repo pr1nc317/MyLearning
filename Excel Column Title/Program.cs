18d7dcfc11f1e2e4736352ec48cd65c1fac0953d
Amit Sharma <amitsharma171094@outlook.com>
2021-12-05 21:58:05 +0530

    Added 'Excel Column Title' problem of Maths

---
+++ Excel Column Title/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Excel_Column_Title
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static string ConvertToTitle(int A)
+        {
+            int quotient = A;
+            var list = new List<char>();
+            while(quotient > 0)
+            {
+                int rem = quotient % 26;
+                quotient /= 26;
+                if (rem == 0)
+                {
+                    rem = 26;
+                    quotient--;
+                }
+                list.Insert(0, (char)(64 + rem));
+            }
+            return new string(list.ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToTitle(54));
+        }
+    }
+}
