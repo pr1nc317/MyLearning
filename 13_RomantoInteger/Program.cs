53245a56fa12e918f17b5ac09aa4765db1c7c4bb
Amit Sharma <amitsharma171094@outlook.com>
2022-09-13 00:46:55 +0530

    13_RomantoInteger LeetCode

---
+++ 13_RomantoInteger/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _13_RomantoInteger
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static int RomanToInt(string s)
+        {
+            var dict = new Dictionary<char, int>() { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+            int integer = dict[s[s.Length - 1]];
+            for (int i = s.Length - 2; i >= 0; i--)
+            {
+                if (dict[s[i]] < dict[s[i + 1]]) integer -= dict[s[i]];
+                else integer += dict[s[i]];
+            }
+            return integer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("MMMCMXCIX"));
+        }
+    }
+}
