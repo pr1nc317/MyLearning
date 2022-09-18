1bf8c33a549035b1949b6f405e9d7487b99f175b
Amit Sharma <amitsharma171094@outlook.com>
2022-09-19 04:25:04 +0530

    Leetcode - LengthofLastWord

---
+++ 58_LengthofLastWord/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _58_LengthofLastWord
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Running;
+    using System;
+    
+    [MemoryDiagnoser]
+    public class Program
+    {
+        [Benchmark]
+        [Arguments(" a bb  ")]
+        public int LengthOfLastWord(string s)
+        {
+            bool charSeen = false;
+            int ans = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    charSeen = true;
+                    ans++;
+                }
+
+                if (s[i] == ' ' && charSeen && ans > 0) break;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>(); 
+            //Console.WriteLine(LengthOfLastWord(" a bb  "));
+        }
+    }
+}
