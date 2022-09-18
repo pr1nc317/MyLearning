9df9901058f8cd9e3bb99e5a75cd3ba22a360f9c
Amit Sharma <amitsharma171094@outlook.com>
2022-09-19 04:20:36 +0530

    LongestCommonPrefix - LeetCode

---
+++ 14_LongestCommonPrefix/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _14_LongestCommonPrefix
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static string LongestCommonPrefix(string[] strs)
+        {
+            Array.Sort(strs);
+            bool match = true; int index = 0;
+            string ans = "";
+            while (match)
+            {
+                if (index < strs[0].Length && strs[0][index].Equals(strs[strs.Length - 1][index]))
+                {
+                    ans += strs[0][index];
+                }
+                else match = false;
+                index++;
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestCommonPrefix(new string[] { "abcd", "abdc", ""}));
+        }
+    }
+}
