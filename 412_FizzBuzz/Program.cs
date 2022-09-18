53459a5d8f605fe284e648edcc6bff98e5423bed
Amit Sharma <amitsharma171094@outlook.com>
2022-09-19 04:24:45 +0530

    Leetcode - FizzBuzz

---
+++ 412_FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _412_FizzBuzz
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static IList<string> FizzBuzz(int n)
+        {
+            var ans = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                if ((i+1) % 3 == 0 && (i + 1) % 5 == 0) ans[i] = "FizzBuzz";
+                else if ((i + 1) % 3 == 0) ans[i] = "Fizz";
+                else if ((i + 1) % 5 == 0) ans[i] = "Buzz";
+                else ans[i] = (i + 1).ToString();
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            FizzBuzz(6).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
