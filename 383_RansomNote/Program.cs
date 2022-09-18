906266951aeff5a510828b1bc642b359dc2ea996
Amit Sharma <amitsharma171094@outlook.com>
2022-09-19 04:24:31 +0530

    Leetcode - RansomNote

---
+++ 383_RansomNote/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _383_RansomNote
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static bool CanConstruct(string ransomNote, string magazine)
+        {
+            bool ans = true;
+            var list = new List<char>();
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                list.Add(magazine[i]);
+            }
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (list.Contains(ransomNote[i]))
+                {
+                    list[list.IndexOf(ransomNote[i])] = '_';
+                    continue;
+                }
+                return false;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanConstruct("abbb", "adcbgb"));
+        }
+    }
+}
