71a471b4f6d35a12e4f5a7ad9a36ae86eb20458c
Amit Sharma <amitsharma171094@outlook.com>
2022-09-25 14:11:57 +0530

    LeetCode_1342_Number_of_Steps_to_Reduce_a_Number_to_Zero

---
+++ 1342_Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1342_Number_of_Steps_to_Reduce_a_Number_to_Zero
+{
+    using System;
+
+    class Program
+    {
+        static int NumberOfSteps(int num)
+        {
+            int ans = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0) num /= 2;
+                else num -= 1;
+                ans++;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfSteps(123));
+        }
+    }
+}
