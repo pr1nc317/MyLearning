fdb0e3147a02ba5ab6baee04c99f3a8bc8ba2477
Amit Sharma <amitsharma171094@outlook.com>
2022-09-26 18:11:12 +0530

    LeetCode_26_Remove_Duplicates_from_Sorted_Array

---
+++ 26_Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _26_Remove_Duplicates_from_Sorted_Array
+{
+    using System;
+
+    class Program
+    {
+        static int RemoveDuplicates(int[] nums)
+        {
+            int j = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+            Console.WriteLine(RemoveDuplicates(new int[] { 1, 1, 2 }));
+        }
+    }
+}
