62a3a523172f6cc4dde8da408625af693ed3dee4
Amit Sharma <amitsharma171094@outlook.com>
2022-09-26 19:08:04 +0530

    LeetCode_70_Climbing_Stairs

---
+++ 70_Climbing Stairs/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _70_Climbing_Stairs
+{
+    using System;
+
+    class Program
+    {
+        static int ClimbStairs(int n)
+        {
+            int last = 1; int lastButOne = 0; int ans = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                ans = last + lastButOne;
+                lastButOne = last;
+                last = ans;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(5));
+        }
+    }
+}
