9e8a950adf0545595424356864c71afec43aa7cb
Amit Sharma <amitsharma171094@outlook.com>
2022-10-11 03:32:54 +0530

    InterviewBit - Word_Count

---
+++ Word Count/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Word_Count
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A)
+        {
+            int count = 0; bool charSeen = false;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == ' ' && charSeen)
+                {
+                    count++;
+                    charSeen = false;
+                }
+                else if (A[i] != ' ')
+                {
+                    charSeen = true;
+                }
+            }
+            if (charSeen)
+            {
+                count++;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("    "));
+        }
+    }
+}
