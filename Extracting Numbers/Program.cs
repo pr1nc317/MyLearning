db1b9f41bac6ef3ac3b175883ae6972292cdc978
Amit Sharma <amitsharma171094@outlook.com>
2022-10-11 03:32:25 +0530

    InterviewBit - Extracting_Numbers

---
+++ Extracting Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Extracting_Numbers
+{
+    using System;
+
+    class Program
+    {
+        public static long Solve(string A)
+        {
+            long sum = 0; long k = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] <= 57 && A[i] >= 48)
+                {
+                    k = k * 10 + A[i]-48;
+                }
+                else
+                {
+                    sum += k;
+                    k = 0;
+                }
+            }
+            if (k != 0)
+            {
+                sum += k;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("123def3"));
+        }
+    }
+}
