bda46b614441998cea09e2abf6bee6b5614d696d
Amit Sharma <amitsharma171094@outlook.com>
2022-10-16 04:22:48 +0530

    IB - Greater_than_All

---
+++ Greater than All/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Greater_than_All
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int Solve(List<int> A)
+        {
+            int count = 1;
+            int max = A[0];
+            for (int i = 1; i < A.Count; i++)
+            {
+                if (A[i] > max)
+                {
+                    count++;
+                    max = A[i];
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(new List<int> { 1,1,3,3,1,2,4}));
+        }
+    }
+}
