7fc1b57605e8539b16ca2202de2a6c37fbd7e8cc
Amit Sharma <amitsharma171094@outlook.com>
2022-10-15 22:29:48 +0530

    InterviewBit - Chips_Factory

---
+++ Chips Factory/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Chips_Factory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static List<int> Solve(List<int> A)
+        {
+            #region using LINQ (simple and easy)
+            int count = A.Where(x => x == 0).Count();
+            A.RemoveAll(x => x == 0);
+            while (count > 0)
+            {
+                A.Add(0);
+                count--;
+            }
+            return A;
+            #endregion
+        }
+        static void Main(string[] args)
+        {
+            Solve(new List<int> { 1, 2, 0, 4, 0, 5 }).ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
