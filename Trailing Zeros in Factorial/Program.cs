38d217f582b5db1b0ed0fba2e7d97d2e1e6e8178
Amit Sharma <amitsharma171094@outlook.com>
2022-10-19 02:57:29 +0530

    IB - Trailing_Zeros_in_Factorial

---
+++ Trailing Zeros in Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Trailing_Zeros_in_Factorial
+{
+    using System;
+
+    class Program
+    {
+        public static int TrailingZeros(int A)
+        {
+            //int count = 0;
+            //for (int i = 1; i < 70; i++)
+            //{
+            //    count += A / (int)Math.Pow(5, i);
+            //}
+            //return count;
+
+            int count = 0;
+            while (A > 4)
+            {
+                count += A / 5;
+                A /= 5;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TrailingZeros(9446865));
+        }
+    }
+}
