0758dac418f85acadfe6bc2045402aa3aabe5c0a
Amit Sharma <amitsharma171094@outlook.com>
2022-10-27 03:29:35 +0530

    IB - Last_digit_K_count

---
+++ Last digit K count/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Last_digit_K_count
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A, int B, int C)
+        {
+            int count = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                if (A + i > B)
+                {
+                    return count;
+                }
+                if ((A + i) % 10 == C)
+                {
+                    count++;
+                    A = A + i;
+                    break;
+                }
+            }
+            count += (B - A) / 10;
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(11, 11, 1));
+        }
+    }
+}
