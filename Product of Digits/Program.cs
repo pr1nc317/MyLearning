c0630113890a86e9ab01c4d8094e091cb39f87c8
Amit Sharma <amitsharma171094@outlook.com>
2022-10-27 16:25:49 +0530

    IB - Product_of_Digits

---
+++ Product of Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Product_of_Digits
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A)
+        {
+            int prod = 1;
+            var str = A.ToString();
+            if (str.Contains("0"))
+            {
+                return 0;
+            }
+            foreach (var item in str)
+            {
+                prod *= Convert.ToInt32(item.ToString());
+            }
+            return prod;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(999999999));
+        }
+    }
+}
