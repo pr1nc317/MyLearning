a126eaa9f0a2b07f3b62ccaa63e21a1ef9a79424
Amit Sharma <amitsharma171094@outlook.com>
2022-10-28 00:59:34 +0530

    IB - Round_Table

---
+++ Round Table/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Round_Table
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A)
+        {
+            int mod = 1000000007;
+            var ans = 1L;
+            for (int i = 1; i <= A; i++)
+            {
+                ans = (ans * i) % mod;
+            }
+            return (int)((ans * 2) % mod);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(1000));
+        }
+    }
+}
