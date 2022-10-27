fc562e061c071b04e5e813ad5cc4bb9dc88a3195
Amit Sharma <amitsharma171094@outlook.com>
2022-10-28 01:29:10 +0530

    IB - Odd_Even_Rule

---
+++ Odd Even Rule/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Odd_Even_Rule
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static int Solve(List<int> A, int B, int C)
+        {
+            if (B % 2 == 0)
+            {
+                return A.Where(x => x % 2 != 0).Count() * C;
+            }
+            else return A.Where(x => x % 2 == 0).Count() * C;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(new List<int> { 1,2,3}, 31, 100));
+        }
+    }
+}
