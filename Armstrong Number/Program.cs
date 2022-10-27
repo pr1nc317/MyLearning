f1d4c0ff4a0ce35d9431e13b2d5f95193edd69b1
Amit Sharma <amitsharma171094@outlook.com>
2022-10-28 01:59:10 +0530

    IB - Armstrong_Number

---
+++ Armstrong Number/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Armstrong_Number
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A)
+        {
+            var str = A.ToString();
+            double sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum += Math.Pow(Convert.ToInt32(str[i].ToString()), str.Length);
+                if (sum > A)
+                {
+                    return 0;
+                }
+            }
+            if (sum == A)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(123));
+        }
+    }
+}
