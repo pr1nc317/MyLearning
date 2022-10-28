0c6661a9cab0be6acdce56294d5d283e96e55b65
Amit Sharma <amitsharma171094@outlook.com>
2022-10-28 15:33:09 +0530

    IB - Leap_Year

---
+++ Leap Year/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Leap_Year
+{
+    using System;
+
+    class Program
+    {
+        public static int IsLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsLeapYear(2000));
+        }
+    }
+}
