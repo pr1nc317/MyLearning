51602893d1a3a885e3b2bdc3556129b786d9d83e
Amit Sharma <amitsharma171094@outlook.com>
2022-11-02 00:20:44 +0530

    IB - Square_Root_of_Integer

---
+++ Square Root of Integer/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Square_Root_of_Integer
+{
+    using System;
+
+    class Program
+    {
+        public static int Sqrt(int A)
+        {
+            long start = 0;
+            long end = A;
+            while (start <= end)
+            {
+                long mid = (start + end) / 2;
+                if (mid * mid == A) return (int)mid;
+                else if (mid * mid > A)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            return (int)end;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sqrt(2147483647));
+        }
+    }
+}
