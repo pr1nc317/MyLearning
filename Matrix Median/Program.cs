b9eaad1a9b575020ddffd65b8e4f402c0c66853d
Amit Sharma <amitsharma171094@outlook.com>
2022-11-06 23:19:40 +0530

    IB - Matrix_Median - Change in Impl

--- Matrix Median/Program.cs
+++ Matrix Median/Program.cs
@@ -20,7 +20,7 @@
 
             int rows = A.Count;
             int cols = A[0].Count;
-            int desired = (rows * cols + 1) / 2;
+            int desired = ((rows * cols) / 2) + 1;
 
             while (min < max)
             {
@@ -29,18 +29,25 @@
                 int get;
                 for (int i = 0; i < rows; i++)
                 {
-                    get = Array.BinarySearch(GetRow(A, i), mid);
-                    if (get < 0)
-                    {
-                        get = Math.Abs(get) - 1;
-                    }
-                    else
-                    {
-                        while (get < GetRow(A,i).Length && A[i][get] == mid)
-                        {
-                            get += 1;
-                        }
-                    }
+                    #region Method 1 to calculate count of elements <= mid
+                    //get = Array.BinarySearch(GetRow(A, i), mid);
+                    //if (get < 0)
+                    //{
+                    //    get = Math.Abs(get) - 1;
+                    //}
+                    //else
+                    //{
+                    //    while (get < GetRow(A,i).Length && A[i][get] == mid)
+                    //    {
+                    //        get += 1;
+                    //    }
+                    //}
+                    #endregion
+
+                    #region Method 2 to calculate count of elements <= mid
+                    var arr = GetRow(A, i);
+                    get = CountOfEleLessThanEqualToMid(arr, mid);
+                    #endregion
                     place += get;
                 }
                 if (place < desired) min = mid + 1;
@@ -59,16 +66,42 @@
             return arr;
         }
 
+        public static int CountOfEleLessThanEqualToMid(int[] A, int num)
+        {
+            int start = 0;
+            int end = A.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (A[mid] == num)
+                {
+                    if (mid < A.Length - 1)
+                        if (A[mid + 1] > num) return mid + 1;
+                        else start = mid + 1;
+                    else return A.Length;
+                }
+                else if (A[mid] < num)
+                    start = mid + 1;
+                else if (A[mid] > num)
+                    if (mid > 0)
+                        if (A[mid - 1] == num) return mid;
+                        else end = mid - 1;
+                    else return 0;
+            }
+            return start;
+        }
+
         static void Main(string[] args)
         {
-            //Console.WriteLine(FindMedian(new List<List<int>> { 
-            //    new List<int> { 1, 3, 5},
-            //    new List<int> { 2, 6, 9},
-            //    new List<int> { 3, 6, 9}
-            //}));
+            Console.WriteLine(FindMedian(new List<List<int>> {
+                new List<int> { 1, 3, 5},
+                new List<int> { 2, 6, 9},
+                new List<int> { 3, 6, 9}
+            }));
             Console.WriteLine(FindMedian(new List<List<int>> {
                 new List<int> { 5, 17, 100 }
             }));
+            //Console.WriteLine(CountOfEleLessThanEqualToMid(new int[] { 1,2,3,4,6,7,8,9,10}, 0));
         }
     }
 }
