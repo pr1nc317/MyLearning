fd81f488e21d229268a0732a9d92f78623c8af80
Amit Sharma <amitsharma171094@outlook.com>
2022-11-06 23:42:32 +0530

    IB - Allocate_Books

---
+++ Allocate Books/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Allocate_Books
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int Books(List<int> A, int B)
+        {
+            if (B > A.Count) return -1;
+            int start = int.MinValue;
+            int end = 0;
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (A[i] > start) start = A[i];
+                end += A[i];
+            }
+            int result = 0;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (IsFfeasible(A, B, mid))
+                {
+                    result = mid;
+                    end = mid - 1;
+                }
+                else start = mid + 1;
+            }
+            return result;
+        }
+
+        public static bool IsFfeasible(List<int> A, int students, int mid)
+        {
+            int st = 1;
+            int curr = 0;
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (curr + A[i] <= mid)
+                    curr += A[i];
+                else
+                {
+                    curr = A[i];
+                    st++;
+                }
+                if (st > students) return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Books(new List<int> { 12, 34, 67, 90 }, 2));
+        }
+    }
+}
