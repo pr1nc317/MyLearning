4146f303b2ef0cee92c92082037cadca037e748b
Amit Sharma <amitsharma171094@outlook.com>
2022-11-25 01:11:07 +0530

    IB - Serialize

---
+++ Serialize/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Serialize
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Program
+    {
+        public static string Serialize(List<string> A)
+        {
+            StringBuilder strbld = new StringBuilder();
+            foreach (var item in A)
+            {
+                strbld.Append(item + item.Length + "~");
+            }
+            return strbld.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Serialize(new List<string> { "scaler", "academy" }));
+        }
+    }
+}
