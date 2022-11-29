88338c8d7e414d37df649dd09b8a798c77a15956
Amit Sharma <amitsharma171094@outlook.com>
2022-11-30 02:35:00 +0530

    IB - Length_of_Last_Word

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Length_of_Last_Word
+{
+    using System;
+
+    class Program
+    {
+        public static int LengthOfLastWord(string A)
+        {
+            bool firstAlphabetContacted = false;
+            int count = 0;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (firstAlphabetContacted)
+                {
+                    if (A[i] == 32) return count;
+                    else count++;
+                }
+                else
+                {
+                    if (A[i] == 32) continue;
+                    else
+                    {
+                        firstAlphabetContacted = true;
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("Hello  "));
+        }
+    }
+}
