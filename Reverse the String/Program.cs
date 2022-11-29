0fa367a4bf0d05fd3ca25ad665b59fbeccd3a9d1
Amit Sharma <amitsharma171094@outlook.com>
2022-11-30 03:01:31 +0530

    IB - Reverse_the_String

---
+++ Reverse the String/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Reverse_the_String
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        public static string Solve(string A)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool charContacted = false;
+            int charContactedIndex = 0;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (charContacted)
+                {
+                    if (A[i] == 32)
+                    {
+                        charContacted = false;
+                        sb.Append(A.Substring(i + 1, charContactedIndex - i) + " ");
+                    }
+                }
+                else
+                {
+                    if (A[i] != 32)
+                    {
+                        charContacted = true;
+                        charContactedIndex = i;
+                    }
+                }
+            }
+            if (charContacted && charContactedIndex >= 0)
+            {
+                sb.Append(A.Substring(0, charContactedIndex + 1));
+            }
+            return sb.ToString().Trim();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(" a    Hello    World  "));
+        }
+    }
+}
