f4148d4a1c2b4d6ef29e8e0932cb0f682ec98e87
Amit Sharma <amitsharma171094@outlook.com>
2022-12-06 02:58:51 +0530

    IB - Convert_to_Palindrome

---
+++ Convert to Palindrome/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Convert_to_Palindrome
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A)
+        {
+            int left = 0; int right = A.Length - 1;
+            int count = 0;
+            bool failedTwice = false;
+            string secondChance = "";
+            while (left < right)
+            {
+                if (A[left] != A[right])
+                {
+                    count++;
+                    if (count == 1)
+                    {
+                        secondChance = A.Substring(left, right - left);
+                    }
+                    if (count > 1)
+                    {
+                        failedTwice = true;
+                    }
+                    left++;
+                }
+                else
+                {
+                    left++; right--;
+                }
+            }
+            if (failedTwice)
+            {
+                left = 0;
+                right = secondChance.Length - 1;
+                while (left < right)
+                {
+                    if (secondChance[left] != secondChance[right]) return 0;
+                    left++; right--;
+                }
+            }
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("baccaba"));
+        }
+    }
+}
