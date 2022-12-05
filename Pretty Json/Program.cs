694450f2bcea57c0ba490ebad3de43d361f39c14
Amit Sharma <amitsharma171094@outlook.com>
2022-12-05 19:53:28 +0530

    insignificant change

--- Pretty Json/Program.cs
+++ Pretty Json/Program.cs
@@ -13,7 +13,6 @@
             int indentCount = 0; 
             for (int i = 0; i < A.Length; i++)
             {
-                var character = A[i];
                 if (A[i] == '{' || A[i] == '[')
                 {
                     if (sb.Length > 0)                    
