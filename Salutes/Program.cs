6661ff30cfd7a3803694cedbaead85d712cd99ea
Amit Sharma <amitsharma171094@outlook.com>
2022-12-07 02:55:57 +0530

    IB - Salutes

---
+++ Salutes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Salutes
+{
+    using System;
+
+    class Program
+    {
+        public static long CountSalutes(string A)
+        {
+            int rCount = 0;
+            long ans = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == '>')
+                {
+                    rCount++;
+                }
+                else
+                {
+                    ans += rCount;
+                }
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSalutes("><><"));
+        }
+    }
+}
