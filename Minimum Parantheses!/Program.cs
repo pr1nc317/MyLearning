553cb7a416f57046e12ef871d041813b20068970
Amit Sharma <amitsharma171094@outlook.com>
2022-12-06 21:18:49 +0530

    IB - Minimum_Parantheses_

---
+++ Minimum Parantheses!/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Minimum_Parantheses_
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A)
+        {
+            int i = 0; int count = 0;
+            int ans = 0;
+            while (i < A.Length)
+            {
+                if (count < 0 && A[i] == '(')
+                {
+                    ans += Math.Abs(count);
+                    count = 0;
+                }
+                if (A[i] == '(')
+                {
+                    count++;
+                }
+                else count--;
+                i++;
+            }
+            return ans + Math.Abs(count);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("())"));
+        }
+    }
+}
