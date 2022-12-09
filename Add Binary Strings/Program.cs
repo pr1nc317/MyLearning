03efc58563da80e728e32cf4233f35486ad85afc
Amit Sharma <amitsharma171094@outlook.com>
2022-12-10 02:29:43 +0530

    IB - Add_Binary_Strings

---
+++ Add Binary Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Add_Binary_Strings
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        public static string AddBinary(string A, string B)
+        {
+            if (string.IsNullOrEmpty(B)) return A;
+            if (string.IsNullOrEmpty(A)) return B;
+            if (A.Length < B.Length)
+            {
+                var temp = B;
+                B = A;
+                A = temp;
+            }
+            int carry = 0;
+            StringBuilder sb = new StringBuilder(A);
+            int i;
+            for (i = 0; i < B.Length || (i < A.Length && carry == 1); i++)
+            {
+                int digitA = A[A.Length - 1 - i] == '0' ? 0 : 1;
+                int digitB = i >= B.Length ? 0 : (B[B.Length - 1 - i] == '0' ? 0 : 1);
+                var sum = digitA + digitB + carry;
+                sb[sb.Length - 1 - i] = sum % 2 == 0 ? '0' : '1';
+                carry = sum / 2;
+            }
+            if (carry == 1) sb.Insert(0, "1");
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary("111", "1"));
+        }
+    }
+}
