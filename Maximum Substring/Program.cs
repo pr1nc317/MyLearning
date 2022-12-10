764a65bb7573a863be7a9c5fe24da68c2f6e2175
Amit Sharma <amitsharma171094@outlook.com>
2022-12-11 01:29:47 +0530

    IB - Maximum_Substring

---
+++ Maximum Substring/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Maximum_Substring
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A, int B)
+        {
+            int maxA = 0;
+            for (int i = 0; i < A.Length; i+=B)
+            {
+                int count = 0;
+                for (int j = i; j < i + B && j < A.Length; j++)
+                {
+                    if (A[j] == 'a') count++;
+                }
+                if (count > maxA) maxA = count;
+            }
+            return maxA;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("bbbabaaaaaa", 15));
+        }
+    }
+}
