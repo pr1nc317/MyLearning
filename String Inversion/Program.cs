232f2148e3b444a774afa13ab53c093c81fc04b9
Amit Sharma <amitsharma171094@outlook.com>
2022-12-11 01:42:34 +0530

    IB - String Inversion

---
+++ String Inversion/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace String_Inversion
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        public static string Solve(string A)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in A)
+            {
+                if (item - 'a' >= 0)
+                {
+                    sb.Append(item.ToString().ToUpper());
+                }
+                else sb.Append(item.ToString().ToLower());
+            }
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("InterviewBit"));
+        }
+    }
+}
