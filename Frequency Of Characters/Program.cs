f1232166f486fa140f8d9bddf1733158cdb8dbc2
Amit Sharma <amitsharma171094@outlook.com>
2022-12-11 01:18:34 +0530

    IB - Frequency_Of_Characters

---
+++ Frequency Of Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Frequency_Of_Characters
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static List<int> Solve(string A)
+        {
+            var freq = new List<int>();
+            for (int i = 0; i < 26; i++)
+            {
+                freq.Add(0);
+            }
+            foreach (var item in A)
+            {
+                freq[item - 'a']++;
+            }
+            return freq;
+        }
+
+        static void Main(string[] args)
+        {
+            Solve("abcdefghijklmnopqrstuvwxyz").ForEach(x => Console.Write(x + " "));
+            Solve("interviewbit").ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
