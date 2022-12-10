78a464eeb9de1c52bb1e4268c28f3f658e41f054
Amit Sharma <amitsharma171094@outlook.com>
2022-12-11 01:36:23 +0530

    IB - Palindromic_Words

---
+++ Palindromic Words/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Palindromic_Words
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A)
+        {
+            var arr = A.Split(" ");
+            int countOfPalindromes = 0;
+            foreach (var item in arr)
+            {
+                int i = 0;
+                int j = item.Length - 1;
+                for (; i < j; i++, j--)
+                {
+                    if (item[i] != item[j])
+                        break;
+                }
+                if (i >= j) countOfPalindromes++;
+            }
+            return countOfPalindromes;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("the fastest racecar"));
+        }
+    }
+}
