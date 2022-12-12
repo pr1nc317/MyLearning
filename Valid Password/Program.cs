08c6e72d09b55d17f1f80668f1ebff113142d244
Amit Sharma <amitsharma171094@outlook.com>
2022-12-12 21:54:30 +0530

    IB - Valid_Password

---
+++ Valid Password/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Valid_Password
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(string A)
+        {
+            if (A.Length < 8 || A.Length > 15) return 0;
+            bool zeroToNine = false;
+            bool smallChar = false;
+            bool bigChar = false;
+            bool specialChar = false;
+            foreach (var item in A)
+            {
+                if (char.IsDigit(item)) zeroToNine = true;
+                if (item - 'a' >= 0 && item - 'a' < 26) smallChar = true;
+                if (item - 'A' >= 0 && item - 'A' < 26) bigChar = true;
+                if (item == '!' || item == '@' || item == '#' || item == '$' || item == '%' || item == '&' || item == '*')
+                    specialChar = true;
+            }
+            return zeroToNine & smallChar & bigChar & specialChar ? 1 : 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("uok9$mpc"));
+        }
+    }
+}
