edbbc9d8feb70686cd9c06d3700a2b5547b0402e
Amit Sharma <amitsharma171094@outlook.com>
2022-12-21 01:20:38 +0530

    IB - Number_Of_1_Bits

---
+++ Number Of 1 Bits/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Number_Of_1_Bits
+{
+    using System;
+
+    class Program
+    {
+        public static int NumSetBits(uint A)
+        {
+            int count = 0;
+            while (A != 0)
+            {
+                A &= A - 1;
+                count++;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumSetBits(11));
+        }
+    }
+}
