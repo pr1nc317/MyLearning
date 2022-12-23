3e65101bde7d714613a7ea643545f1e016db09e7
Amit Sharma <amitsharma171094@outlook.com>
2022-12-23 18:50:44 +0530

    IB - Reverse_Bits

---
+++ Reverse Bits/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Reverse_Bits
+{
+    using System;
+
+    class Program
+    {
+        public static long Solve(long A)
+        {
+            //int pow = 31;
+            //long ans = 0;
+            //while (A != 0)
+            //{
+            //    if (A % 2 != 0)
+            //    {
+            //        ans += (long)Math.Pow(2, pow);
+            //    }
+            //    A >>= 1;
+            //    pow--;
+            //}
+            //return ans;
+
+            long ans = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                ans <<= 1;
+                if ((A & (1 << i)) != 0)
+                {
+                    ans |= 1;
+                }
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(4));
+        }
+    }
+}
