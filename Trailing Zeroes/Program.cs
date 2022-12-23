a45bad5cfa755b220e4da16ef105f60c1ce973ab
Amit Sharma <amitsharma171094@outlook.com>
2022-12-23 13:06:46 +0530

    IB - Trailing_Zeroes

---
+++ Trailing Zeroes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Trailing_Zeroes
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A)
+        {
+            int count = 0;
+            while (A % 2 == 0)
+            {
+                A >>= 1;
+                count++;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(8));
+        }
+    }
+}
