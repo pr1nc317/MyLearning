bad6bb9a867f610f42ca42b957e221f8c75b5464
Amit Sharma <amitsharma171094@outlook.com>
2022-12-24 21:00:06 +0530

    IB - Single_Number

---
+++ Single Number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Single_Number
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int SingleNumber(List<int> A)
+        {
+            int temp = A[0];
+            for (int i = 1; i < A.Count; i++)
+            {
+                if (i % 2 == 0)
+                    temp -= A[i];
+                else temp += A[i];
+            }
+            return Math.Abs(temp);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingleNumber(new List<int> { 1, 2, 2, 3, 1 }));
+            Console.WriteLine(SingleNumber(new List<int> { 1, 2, 2 }));
+            Console.WriteLine(SingleNumber(new List<int> { 1 }));
+        }
+    }
+}
