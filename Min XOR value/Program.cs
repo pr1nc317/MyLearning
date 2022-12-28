258474712a304ddb42beb36e49492498acd4a571
Amit Sharma <amitsharma171094@outlook.com>
2022-12-28 16:18:24 +0530

    IB - Min_XOR_value

---
+++ Min XOR value/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Min_XOR_value
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static int FindMinXor(List<int> A)
+        {
+            /*
+             * Two ways to solve:
+             * 1. Two loops - O(N^2)
+             * 2. Sort the list, then xor every consecutive pair - O(NLogN + N) = O(NLogN) : https://www.youtube.com/watch?v=OZ2jghS0t24
+             */
+
+            #region Approach 2
+            var minXor = int.MaxValue;
+            A.Sort();
+            for (int i = 0; i < A.Count - 1; i++)
+            {
+                var temp = A[i] ^ A[i + 1];
+                if (temp < minXor)
+                    minXor = temp;
+            }
+            return minXor;
+            #endregion
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMinXor(new List<int> { 0, 7, 5, 2 }));
+        }
+    }
+}
