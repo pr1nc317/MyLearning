a4233b1b71a94e6983e7d96ef8993905bd014632
Amit Sharma <amitsharma171094@outlook.com>
2022-12-30 01:24:34 +0530

    IB - Bit_Flipping

---
+++ Bit Flipping/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Bit_Flipping
+{
+    using System;
+
+    class Program
+    {
+        public static int Solve(int A)
+        {
+            int ans = 0;
+            int count = 0;
+            while (A != 0)
+            {
+                int lsb = (A & 1);
+                int temp = lsb ^ 1;
+                temp <<= count;
+                A >>= 1;
+                count++;
+                ans |= temp;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(5));
+        }
+    }
+}
