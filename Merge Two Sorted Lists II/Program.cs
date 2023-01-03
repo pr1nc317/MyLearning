577203e31538730189ebb58ce46d2d2788ee386d
Amit Sharma <amitsharma171094@outlook.com>
2023-01-03 21:31:54 +0530

    IB - Merge_Two_Sorted_Lists_II

---
+++ Merge Two Sorted Lists II/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Merge_Two_Sorted_Lists_II
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static void Merge(List<int> a, List<int> b)
+        {
+            int i = 0, j = 0;
+            while (i < a.Count && j < b.Count)
+            {
+                if (a[i] <= b[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    a.Insert(i, b[j]);
+                    j++; i++;
+                }
+            }
+            while (j < b.Count)
+            { 
+                a.Add(b[j]);
+                j++; 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Merge(new List<int> { 2 }, new List<int> { 3, 5 });
+        }
+    }
+}
