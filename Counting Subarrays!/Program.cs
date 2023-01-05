d2f5f58016643ba3c2225ce89fbcc568d04ce1fb
Amit Sharma <amitsharma171094@outlook.com>
2023-01-06 01:52:44 +0530

    IB - Counting_Subarrays

---
+++ Counting Subarrays!/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Counting_Subarrays_
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static int Solve(List<int> A, int B)
+        {
+            int i = 0;
+            int j = 0;
+            int ans = 0;
+            int currSum = 0;
+            while (i < A.Count)
+            {
+                if (A[i] < B)
+                {
+                    ans++;
+                }
+                else if (A[i] >= B)
+                {
+                    i++;
+                    j = i;
+                    continue;
+                }
+                currSum += A[i];
+                while (currSum >= B)
+                {
+                    currSum -= A[j];
+                    j++;
+                }
+                if (i > j)
+                    ans += i - j;
+                i++;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(new List<int> { 2, 5, 6 }, 10));
+            Console.WriteLine(Solve(new List<int> { 1, 11, 2, 3, 15 }, 10));
+            Console.WriteLine(Solve(new List<int> { 2, 5, 6, 4, 8, 10 }, 15));
+        }
+    }
+}
