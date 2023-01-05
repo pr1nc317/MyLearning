dc69f604d4e05bc1d92e3021f4239d4854ce9c35
Amit Sharma <amitsharma171094@outlook.com>
2023-01-05 21:45:09 +0530

    IB - Maximum_Ones_After_Modification

---
+++ Maximum Ones After Modification/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Maximum_Ones_After_Modification
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static int Solve(List<int> A, int B)
+        {
+            int flip = 0;
+            int i = 0;
+            int j = 0;
+            int ans = 0;
+            while (i < A.Count)
+            {
+                if (A[i] == 0)
+                    flip++;
+                while (flip > B)
+                {
+                    if (A[j] == 0)
+                        flip--;
+                    j++;
+                }
+                ans = Math.Max(ans, i - j + 1);
+                i++;
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve(new List<int> { 1, 0, 0, 1, 1, 0, 1 }, 1));
+            Console.WriteLine(Solve(new List<int> { 1, 0, 0, 1, 0, 1, 0, 1, 0, 1 }, 2));
+        }
+    }
+}
