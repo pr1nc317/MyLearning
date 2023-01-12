7fbfea7cfd92c4ce0528ba987d23cbece18f7f1a
Amit Sharma <amitsharma171094@outlook.com>
2023-01-13 03:52:20 +0530

    IB - Reverse_Linked_List

---
+++ Reverse Linked List/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Reverse_Linked_List
+{
+    using System;
+
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; next = null; }
+        }
+
+        public ListNode ReverseList(ListNode A)
+        {
+            ListNode temp = A.next;
+            A.next = null;
+            while (temp.next != null)
+            {
+                ListNode curr = temp.next;
+                temp.next = A;
+                A = temp;
+                temp= curr;
+            }
+            temp.next = A;
+            return temp;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
