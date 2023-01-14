2e048a35d04997004c443b5fb8f141fb646296af
Amit Sharma <amitsharma171094@outlook.com>
2023-01-15 01:19:13 +0530

    IB - K_Reverse_Linked_List

---
+++ K Reverse Linked List/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace K_Reverse_Linked_List
+{
+    using System;
+
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; next = null; }
+        }
+
+        public static ListNode ReverseList(ListNode A, int B)
+        {
+            if (B == 1) return A;
+            ListNode prev = new ListNode(0);
+            ListNode prevPtr = prev;
+            while (A != null)
+            {
+                int copyB = B;
+                ListNode org = new ListNode(A.val);
+                ListNode orgPtr = org;
+                while (copyB > 1)
+                {
+                    copyB--;
+                    A = A.next;
+                    var newNode = new ListNode(A.val);
+                    orgPtr.next = newNode;
+                    orgPtr = newNode;
+                }
+                A = A.next;
+                var reversed = ReverseList(org);
+                prevPtr.next = reversed;
+                while (prevPtr.next != null)
+                {
+                    prevPtr = prevPtr.next;
+                }
+            }
+            return prev.next;
+        }
+
+        public static ListNode ReverseList(ListNode A)
+        {
+            if (A == null) return null;
+            var temp = A.next;
+            if (temp == null) { return A; }
+            A.next = null;
+            while (temp != null)
+            {
+                var curr = temp.next;
+                temp.next = A;
+                A = temp;
+                temp = curr;
+            }
+            return A;
+        }
+
+        static void Main(string[] args)
+        {
+            //ListNode node7 = new ListNode(7);
+            ListNode node6 = new ListNode(6); //node6.next = node7;
+            ListNode node5 = new ListNode(5); node5.next = node6;
+            ListNode node4 = new ListNode(4); node4.next = node5;
+            ListNode node3 = new ListNode(3); node3.next = node4;
+            ListNode node2 = new ListNode(2); node2.next = node3;
+            ListNode node1 = new ListNode(1); node1.next = node2;
+            var ans = ReverseList(node1, 1);
+            while (ans != null)
+            {
+                Console.WriteLine(ans.val);
+                ans = ans.next;
+            }
+        }
+    }
+}
