06a0e829e4c8dc03f28dcf6873d4e43db7e7f875
Amit Sharma <amitsharma171094@outlook.com>
2023-01-20 14:56:41 +0530

    IB - Reverse_String

---
+++ Reverse String/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Reverse_String
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static string ReverseString(string A)
+        {
+            Stack<char> st = new Stack<char>();
+            foreach (char c in A)
+            {
+                st.Push(c);
+            }
+            string ans = string.Empty;
+            while (st.Count > 0)
+            {
+                ans += st.Pop();
+            }
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("abc"));
+        }
+    }
+}
