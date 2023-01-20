23275b7593735794393f44f5fcf475e756b5d676
Amit Sharma <amitsharma171094@outlook.com>
2023-01-20 14:50:29 +0530

    IB - Generate_all_Parentheses

---
+++ Generate all Parentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Generate_all_Parentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static int IsValid(string A)
+        {
+            Stack<char> st = new Stack<char>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == '(' || A[i] == '{' || A[i] == '[')
+                {
+                    st.Push(A[i]);
+                }
+                else
+                {
+                    if (st.Count > 0)
+                    {
+                        var pop = st.Pop();
+                        if (IsValidPair(pop, A[i]))
+                            continue;
+                        else return 0;
+                    }
+                    else return 0;
+                }
+            }
+            return st.Count == 0 ? 1 : 0;
+        }
+
+        public static bool IsValidPair(char a, char b)
+        {
+            if (a == '(' && b == ')') return true;
+            if (a == '{' && b == '}') return true;
+            if (a == '[' && b == ']') return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("(())"));
+            Console.WriteLine(IsValid("([)]"));
+            Console.WriteLine(IsValid("([]()"));
+            Console.WriteLine(IsValid("([]()){([])}"));
+        }
+    }
+}
