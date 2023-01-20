1c8aaa034867c92def9a440fe8d5439a40bf5f40
Amit Sharma <amitsharma171094@outlook.com>
2023-01-20 15:06:04 +0530

    IB - Balanced_Parantheses_

---
+++ Balanced Parantheses!/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Balanced_Parantheses_
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        public static int Solve(string A)
+        {
+            Stack<char> st = new Stack<char>();
+            foreach (char c in A)
+            {
+                if (c == '(')
+                {
+                    st.Push(')');
+                }
+                else
+                {
+                    if (st.Count == 0) return 0;
+                    else st.Pop();
+                }
+            }
+            return st.Count == 0 ? 1 : 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("(()())"));
+            Console.WriteLine(Solve("(()"));
+        }
+    }
+}
